fd964d470ec73415bd509e6cf474f5be340a395d
Nimeggis <nik.meissner@web.de>
2026-04-01 17:48:07 +0200

    use if instead exception

--- ms365_accountcreator/db_models/__init__.py
+++ ms365_accountcreator/db_models/__init__.py
@@ -33,13 +33,15 @@ def create_db_function():
     APP_LOGGER.info('Database created.')
 
 
+from sqlalchemy import inspect
+
 with APP.app_context():
-    try:
-        registered_email_address.RegisteredEmailAddress.query.first()
-        APP_LOGGER.info('Database tables already exist.')
-    except Exception:
+    inspector = inspect(DB.engine)
+    if 'RegisteredEmailAddress' not in inspector.get_table_names():
         APP_LOGGER.info('Database tables not found, creating them now...')
         create_db_function()
+    else:
+        APP_LOGGER.info('Database tables already exist.')
 
 
 @APP.cli.command('drop_db')
